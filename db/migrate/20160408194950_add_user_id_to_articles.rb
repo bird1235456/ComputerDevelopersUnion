86916ce0e3fb5ad1e0f9bab74a1d9a72dc0c6ed6
CDU <Mark@ComputerDevelopersUnion.com>
2016-04-13 15:55:01 +0000

    Colon incident

--- db/migrate/20160408194950_add_user_id_to_articles.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToArticles < ActiveRecord::Migration
-  def change
-    add_coluun :articles, :user_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160408183007) do
+ActiveRecord::Schema.define(version: 20160413150653) do
 
   create_table "articles", force: :cascade do |t|
-    t.string   "title"
-    t.text     "description"
-    t.datetime "created_at"
-    t.datetime "update_at"
+    t.string "title"
+    t.text   "description"
   end
 
   create_table "users", force: :cascade do |t|
